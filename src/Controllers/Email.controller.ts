5c79051bcc32bd4ad00cc5a57dbb710b0fee81dc
moccia.ant@gmail.com <moccia.ant@gmail.com>
2024-01-07 13:14:54 +0100

    everything ok

--- src/Controllers/Email.controller.ts
+++ src/Controllers/Email.controller.ts
@@ -7,41 +7,66 @@ class EmailController {
     constructor() { }
 
     static async sandEmail(req: Request, res: Response): Promise<void> {
- 
+
         const initService = new GmailService({ id_service: req.params.service })
+
         const serviceInfo = await initService.getServiceInfo()
-        const userInfo = await initService.getUserInfo(String(serviceInfo?.authParams.access_token))
+
+        /**Authentication will move to sandEmail method */
+        if ((serviceInfo instanceof Error)) {
+            res.status(500).json({
+                message: serviceInfo.message
+            })
+            return
+        }
         await initService.authenticate({
-            access_token:String(serviceInfo?.authParams.access_token),
-            refresh_token:String(serviceInfo?.authParams.refresh_token)
+            access_token: String(serviceInfo?.authParams.access_token),
+            refresh_token: String(serviceInfo?.authParams.refresh_token)
         })
+        /**Here are user info like email address */
+        const userInfo = await initService.getUserInfo(String(serviceInfo?.authParams.access_token))
+        const tokenInfo = await initService.getToken(String(serviceInfo?.authParams.access_token))
+
+
         const sand = await initService.sandEmail()
-        console.log(sand);
+        console.log('provider', initService.provider);
 
         res.json(sand)
     }
     static async login(req: Request, res: Response): Promise<void> {
-        const initService = new GmailService()
-        const authUrl = initService.generateAuthUrl()
-        req.internalParams = {
-            int: "value"
-        }
-        res.json({ url: authUrl })
-    }
-    static async saveLogin(req: Request, res: Response): Promise<void> {
+        const service = new GmailService()
+        const authUrl = service.generateAuthUrl(req.params.id_service)
        
+        res.json({ url: authUrl })
+
+    }
+    static async redirect(req: Request, res: Response): Promise<void> {
+        const service_id = req.query.state ? JSON.parse(String(req.query.state)).service_id : undefined
         const gmailService = new GmailService()
 
         try {
-            const resTokens = await gmailService.getTokens(String(req.query.code))
+            const resTokens = await gmailService.getTokensByCode(String(req.query.code))
+            const userInfo = await gmailService.getUserInfo(String(resTokens.tokens.access_token))
+            if (service_id) {
 
-            gmailService.createService({
-                authParams: {
-                    access_token: String(resTokens.tokens.access_token),
-                    refresh_token: String(resTokens.tokens.refresh_token)
-                }
-            })
+                gmailService.updateService(service_id, {
+                    authParams: {
+                        access_token: String(resTokens.tokens.access_token),
+                        refresh_token: String(resTokens.tokens.refresh_token)
+                    }
+                })
+            } else {
 
+                gmailService.createService({
+                    provider: 'gmail',
+                    email: userInfo.email,
+                    authParams: {
+                        access_token: '',
+                        refresh_token: ''
+                    }
+                })
+
+            }
             res.status(200).json({ tokens: resTokens })
         } catch (error) {
             res.status(401).json({ message: "non autorizzato" })
@@ -49,5 +74,3 @@ class EmailController {
     }
 }
 export default EmailController
-
-
